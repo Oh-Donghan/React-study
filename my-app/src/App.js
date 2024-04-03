58d6a9f2e110d34ca7d908ed78cf0c302da15733
Oh-Donghan <odonghan94@gmail.com>
2024-04-03 20:36:24 +0900

    no message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 // import Counter from './components/Counter';
 // import Counter2 from './components/Counter2';
 import Calculator from './components/Calculator';
+import ClassClock from './ClassClock';
+import FuntionClock from './FunctionClock';
 
 function App() {
-  return <Calculator />;
-  // return <div className='App'>
-  //   <Counter />
-  //   <Counter2 />
-  // </div>;
+  return <div className='App'>
+    {/* <Counter />
+    <Counter2 /> */}
+    <Calculator />
+    {/* <ClassClock /> */}
+    {/* <FuntionClock /> */}
+  </div>;
 }
 
 export default App;

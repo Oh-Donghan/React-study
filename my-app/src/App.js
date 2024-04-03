1c5ef995ec642d64fe06e9544b3b1399d1160015
Oh-Donghan <odonghan94@gmail.com>
2024-04-03 16:42:46 +0900

    no message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
+// import Counter2 from './components/Counter2';
+import Calculator from './components/Calculator';
 
 function App() {
-  return <div className='App'>
-    <Counter />
-  </div>;
+  return <Calculator />;
+  // return <div className='App'>
+  //   <Counter />
+  //   <Counter2 />
+  // </div>;
 }
 
 export default App;

7097608f1772326a4e9f0d19d276028ca6ec2487
Oh-Donghan <odonghan94@gmail.com>
2024-04-14 14:14:25 +0900

    props 공부

--- zero-react/first-start-reactPJ/github-issue-project/src/ListContainer.js
+++ zero-react/first-start-reactPJ/github-issue-project/src/ListContainer.js
@@ -1,13 +1,12 @@
-import { useState, useEffect } from "react";
 import styles from "./ListContainer.module.css";
+import { useState } from "react";
 import Button from "./components/Button";
+import ListItem from "./components/ListItem";
+import ListItemLayout from "./components/ListItemLayout";
+import cx from "clsx";
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open ");
-
-  useEffect(() => {
-    console.log(inputValue);
-  }, [inputValue]);
 
   return (
     <div className={styles.listContainer}>
@@ -23,6 +22,65 @@ export default function ListContainer() {
           New Issue
         </Button>
       </div>
+      <OpenClosedFilters />
+      <ListItemLayout className={styles.listFilter}>
+        <div className={styles.filterLists}>
+          <span>Author</span>
+          <span>Label</span>
+          <span>Projects</span>
+          <span>Milestones</span>
+          <span>Assignee</span>
+          <span>Sort</span>
+        </div>
+      </ListItemLayout>
+      <div className={styles.container}>
+        <ListItem
+          badges={[
+            {
+              color: "red",
+              title: "Bug2",
+            },
+          ]}
+        />
+      </div>
     </div>
+  );
+}
+
+function OpenClosedFilters({ data }) {
+  const [isOpenMode, setIsOpenMode] = useState(true);
+  // const data = getData();
+  // const opendData = data.filter((d) => d.state === 'open');
+  // const closedData = data.filter((d) => d.state === 'closed');
+  const openModeDataSize = 1;
+  const CloseModeDataSize = 2;
+
+  return (
+    <>
+      <OpenClosedFilter
+        size={openModeDataSize}
+        state="Open"
+        selected={isOpenMode}
+        onClick={() => setIsOpenMode(true)}
+      />
+      <OpenClosedFilter
+        size={CloseModeDataSize}
+        state="Closed"
+        selected={!isOpenMode}
+        onClick={() => setIsOpenMode(false)}
+      />
+    </>
+  );
+}
+
+function OpenClosedFilter({ size, state, onClick, selected }) {
+  return (
+    <span
+      role="button"
+      className={cx(styles.textFilter, { [styles.selected]: selected })}
+      onClick={onClick}
+    >
+      {size} {state}
+    </span>
   );
 }

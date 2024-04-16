9fe35036e4efb76176ed1dca733cdcfdf946ed9e
Oh-Donghan <odonghan94@gmail.com>
2024-04-16 18:17:50 +0900

    create-issue (깃허브 이슈 API 연동하여 내 페이지에서 깃허브 이슈 생성하기)

---
+++ zero-react/first-start-reactPJ/github-issue-project/src/Hooks.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+export function useForm({
+  initialValues,
+  validate,
+  refs,
+  onSuccess, // 성공했을때
+  onErrors, // 에러가 났을때
+  onSubmit, // 값이 전달될때 어떤 함수/네트워크를 호출할지
+}) {
+  const [inputValues, setInputValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function onChange(e) {
+    const { name, value } = e.target;
+    setInputValues({ ...inputValues, [name]: value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+    const validateResult = validate(inputValues);
+    setErrors(validateResult);
+
+    const errorKeys = Object.keys(validateResult);
+
+    if (errorKeys.length !== 0) {
+      const key = errorKeys[0];
+      alert(validateResult[key]);
+      onErrors();
+      refs[key].current.focus();
+      // ref control
+      setIsSubmitting(false);
+      return;
+    }
+
+    if (errorKeys.length === 0) {
+      await onSubmit();
+      return;
+    }
+  }
+
+  return {
+    inputValues,
+    onChange,
+    isSubmitting,
+    errors,
+    handleSubmit,
+  };
+}
--- zero-react/first-start-reactPJ/github-issue-project/src/pages/CreateIssue.js
+++ zero-react/first-start-reactPJ/github-issue-project/src/pages/CreateIssue.js
@@ -1,48 +1,33 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import cx from "clsx";
 import styles from "./CreateIssue.module.css";
 import Button from "../components/Button";
 import TextField from "../components/TextField";
+import { useForm } from "../Hooks";
+import axios from "axios";
+import { GITHUB_API } from "../api";
 
 export default function CreateIssue() {
   const inputRef = useRef();
   const textareaRef = useRef();
-  const [inputValues, setInputValues] = useState({ title: "", body: "" });
-  const [errors, setErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    setIsSubmitting(true);
-    const validateResult = validate(inputValues);
-    setErrors(validateResult);
-
-    const refs = { title: inputRef, body: textareaRef };
-    const errorKeys = Object.keys(validateResult);
-
-    if (errorKeys.length !== 0) {
-      const key = errorKeys[0];
-      alert(validateResult[key]);
-      refs[key].current.focus();
-      // ref control
-      setIsSubmitting(false);
-      return;
-    }
-
-    if (errorKeys.length === 0) {
-      console.log("Submit 성공");
-    }
-  }
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  function onChange(e) {
-    const { name, value } = e.target;
-    setInputValues({ ...inputValues, [name]: value });
-  }
+  const { isSubmitting, inputValues, onChange, errors, handleSubmit } = useForm(
+    {
+      initialValues: { title: "", body: "" },
+      onSubmit: async () =>
+        await axios.post(
+          `${GITHUB_API}/repos/Oh-Donghan/React-study/issues`,
+          inputValues,
+          {
+            headers: {
+              Authorization: process.env.REACT_APP_GITHUB_TOKEN,
+              "Content-Type": "application/json",
+            },
+          },
+        ),
+      validate,
+      refs: { title: inputRef, body: textareaRef },
+    },
+  );
 
   return (
     <div className={styles.container}>

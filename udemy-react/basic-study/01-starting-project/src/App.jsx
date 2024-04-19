947ffd69b06db6cfb8d10f6ff43292fdd55be752
Oh-Donghan <odonghan94@gmail.com>
2024-04-19 22:33:41 +0900

    react study

--- udemy-react/basic-study/01-starting-project/src/App.jsx
+++ udemy-react/basic-study/01-starting-project/src/App.jsx
@@ -1,14 +1,17 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
-  
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
   return (
     <header>
       <img src={reactImg} alt='Stylized atom' />
@@ -21,12 +24,30 @@ function Header() {
   );
 }
 
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );

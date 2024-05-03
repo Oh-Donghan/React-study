3f1693889de91607716f3ad202143dc187b7ee18
Oh-Donghan <odonghan94@gmail.com>
2024-05-03 22:23:57 +0900

    learning CSS-in-JS

--- udemy-react/basic-study/react-styling/src/components/AuthInputs-styled-component.jsx
+++ udemy-react/basic-study/react-styling/src/components/AuthInputs-styled-component.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react';
+import { styled } from 'styled-components';
 
 import Button from './Button';
 import Input from './Input';
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -24,11 +32,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div
-      id='auth-inputs'
-      className=' w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'
-    >
-      <div className='flex flex-col gap-2 mb-6'>
+    <div id='auth-inputs'>
+      <ControlContainer>
         <Input
           label='Email'
           invalid={emailNotValid}
@@ -36,16 +41,16 @@ export default function AuthInputs() {
           onChange={(event) => handleInputChange('email', event.target.value)}
         />
         <Input
-          label='Password'
+          label="Password"
           invalid={passwordNotValid}
           type='password'
           onChange={(event) =>
             handleInputChange('password', event.target.value)
           }
         />
-      </div>
-      <div className='flex justify-end gap-4'>
-        <button type='button' className='text-amber-400 hover:text-amber-500'>
+      </ControlContainer>
+      <div className='actions'>
+        <button type='button' className='text-button'>
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>

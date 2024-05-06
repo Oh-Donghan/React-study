79025e814cbb2567b9134ad075dc317bb34ac82c
Oh-Donghan <odonghan94@gmail.com>
2024-05-06 22:47:42 +0900

    management project with Ref and State

--- udemy-react/basic-study/project-management/src/App.jsx
+++ udemy-react/basic-study/project-management/src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+import NoProjectSelected from './components/NoProjectSelected';
+import ProjectSidebar from './components/ProjectSidebar';
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+  
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className='h-screen my-8 flex gap-8'>
+      <ProjectSidebar />
+      <NoProjectSelected />
+    </main>
   );
 }
 

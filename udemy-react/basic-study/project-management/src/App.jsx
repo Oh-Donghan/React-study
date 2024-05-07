c8f2293404476f1b3d514fdba58daaac1bc86ecf
Oh-Donghan <odonghan94@gmail.com>
2024-05-07 22:47:11 +0900

    Receive values through callback function

--- udemy-react/basic-study/project-management/src/App.jsx
+++ udemy-react/basic-study/project-management/src/App.jsx
@@ -1,17 +1,53 @@
 import { useState } from 'react';
 import NoProjectSelected from './components/NoProjectSelected';
 import ProjectSidebar from './components/ProjectSidebar';
+import NewProject from './components/NewProject';
 
 function App() {
   const [projectState, setProjectState] = useState({
-    selectedProject: undefined,
+    // undefined 는 아무것도 추가하지 않는 상태
+    // null 은 새로운 프로젝트를 추가하는 상태
+    selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      }
+      
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      }
+    });
+  }
+
+  console.log(projectState);
   
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }

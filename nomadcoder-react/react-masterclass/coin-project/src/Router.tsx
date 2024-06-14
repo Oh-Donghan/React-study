c65bd7ea129d24c35c15f9a3b0ee50a8a0848da6
Oh-Donghan <odonghan94@gmail.com>
2024-06-14 23:05:26 +0900

    publish using gh-pages

--- nomadcoder-react/react-masterclass/coin-project/src/Router.tsx
+++ nomadcoder-react/react-masterclass/coin-project/src/Router.tsx
@@ -1,37 +1,37 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Coins from './routes/Coins';
 import Coin from './routes/Coin';
 import Chart from './routes/Chart';
 import Price from './routes/Price';
 
 // 최신 라우터 사용법
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Coins />,
-  },
-  {
-    path: '/:coinId',
-    element: <Coin />,
-    children: [
-      {
-        path: 'chart',
-        element: <Chart />,
-      },
-      {
-        path: 'price',
-        element: <Price />,
-      }
-    ]
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Coins />,
+//   },
+//   {
+//     path: '/:coinId',
+//     element: <Coin />,
+//     children: [
+//       {
+//         path: 'chart',
+//         element: <Chart />,
+//       },
+//       {
+//         path: 'price',
+//         element: <Price />,
+//       }
+//     ]
+//   }
+// ]);
 
-export default router;
+// export default router;
 
-/* 가장 많이 사용되는 라우터 사용법
+// 가장 많이 사용되는 라우터 사용법
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Coins />} />
         <Route path='/:coinId' element={<Coin />}>
@@ -39,9 +39,8 @@ function Router() {
           <Route path='chart' element={<Chart />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
 export default Router;
-*/

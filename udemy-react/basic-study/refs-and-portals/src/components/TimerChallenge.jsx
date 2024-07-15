e512bbba22a2160ee91e739e85d78a82c902e9b1
Oh-Donghan <odonghan94@gmail.com>
2024-07-15 12:58:46 +0900

    Studying Ref

--- udemy-react/basic-study/refs-and-portals/src/components/TimerChallenge.jsx
+++ udemy-react/basic-study/refs-and-portals/src/components/TimerChallenge.jsx
@@ -11,11 +11,8 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    dialog.current.open();
-  }
-
-  function handleReset() {
     setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
   }
 
   function handleStart() {
@@ -31,12 +28,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal
-        ref={dialog}
-        targetTime={targetTime}
-        remainingTime={timeRemaining}
-        onReset={handleReset}
-      />
+      <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
       <section className='challenge'>
         <h2>{title}</h2>
         <p className='challenge-time'>

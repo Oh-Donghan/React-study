d2d94c385c52339deb3b2f1059c4bf7a9f0613a4
Oh-Donghan <odonghan94@gmail.com>
2024-07-17 23:19:24 +0900

    Studying Optimization with React Hooks and using Million.js

--- udemy-react/basic-study/optimization-techniques/src/components/Counter/Counter.jsx
+++ udemy-react/basic-study/optimization-techniques/src/components/Counter/Counter.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
+import { memo, useState, useCallback, useMemo, useEffect } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log('Calculating if is prime number', 2, 'other');
+
   if (number <= 1) {
     return false;
   }
@@ -27,23 +25,51 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+// ConfigureCounter 컴포넌트를 App컴포넌트에서 분리 했기 때문에 (기존 App컴포넌트의 상태값들)
+// memo는 여기에 더이상 필요가 없지만 사용법 확인을 위해 남겨둠
+// memo는 컴포넌트를 메모이제이션하여 동일한 props로 렌더링 될 때 다시 렌더링 되지 않는다.
+// 자주 바뀌지 않는 props를 받는 컴포넌트에 사용하여 불필요한 렌더링을 피할 수 있다.
+const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
 
-  const [counter, setCounter] = useState(initialCount);
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
 
-  function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
-  }
+  // useEffect(() => {
+  //   setCounterChanges([ { value: initialCount, id: Math.random() * 1000 }])
+  // }, [initialCount])
 
-  function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([
+    { value: initialCount, id: Math.random() * 1000 },
+  ]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
+
+  const handleDecrement = useCallback(function handleDecrement() {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: -1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
+
+  const handleIncrement = useCallback(function handleIncrement() {
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: 1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
 
   return (
-    <section className="counter">
-      <p className="counter-info">
+    <section className='counter'>
+      <p className='counter-info'>
         The initial counter value was <strong>{initialCount}</strong>. It{' '}
         <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
       </p>
@@ -51,11 +77,14 @@ export default function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
-}
+});
+
+export default Counter;

62c0e809e42a08e07ec8fa698f15bdc895cbd79e
Oh-Donghan <odonghan94@gmail.com>
2024-07-19 22:00:11 +0900

    Studying class-based-component

--- udemy-react/basic-study/class-based-component/src/components/User.js
+++ udemy-react/basic-study/class-based-component/src/components/User.js
@@ -1,7 +1,20 @@
+import { Component } from "react";
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// 클래스형 컴포넌트
+class User extends Component {
+  componentWillUnmount() {
+    console.log('User will unmount!');
+  }
+  
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// 함수형 컴포넌트
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;

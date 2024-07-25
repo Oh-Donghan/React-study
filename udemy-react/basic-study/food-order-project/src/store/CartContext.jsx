433b2fc97612a388306ea8b81b25d673b69e75b6
Oh-Donghan <odonghan94@gmail.com>
2024-07-25 18:42:44 +0900

    Make http Custom hook

--- udemy-react/basic-study/food-order-project/src/store/CartContext.jsx
+++ udemy-react/basic-study/food-order-project/src/store/CartContext.jsx
@@ -13,7 +13,7 @@ function cartReducer(state, action) {
       (item) => item.id === action.item.id
     );
 
-    const updateItems = [...state.items];
+    const updatedItems = [...state.items];
 
     if (existingCartItemIndex > -1) {
       const existingItem = state.items[existingCartItemIndex];
@@ -21,24 +21,58 @@ function cartReducer(state, action) {
         ...existingItem,
         quantity: existingItem.quantity + 1,
       };
-      updatedItem[existingCartItemIndex] = updateItems;
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
-      updateItems.push({ ...action.item, quantity: 1 });
+      updatedItems.push({ ...action.item, quantity: 1 });
     }
 
-    return { ...state, items: updateItems };
+    return { ...state, items: updatedItems };
   }
   if (action.type === 'REMOVE_ITEM') {
     // ... remove an item from the state
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,  
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
   }
 
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  function addItem(item) {
+    dispatchCartAction({ type: 'ADD_ITEM', item: item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: 'REMOVE_ITEM', id: id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem: addItem,
+    removeItem: removeItem,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;

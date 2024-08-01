b0025e560db42c59cea3a73f116e9476f5f2a3bf
Oh-Donghan <odonghan94@gmail.com>
2024-08-01 18:17:40 +0900

    studying react-router

--- udemy-react/library/react-router/src/App.js
+++ udemy-react/library/react-router/src/App.js
@@ -1,5 +1,29 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductPage from './pages/Products';
+
+// * 이전 방식의 라우터
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductPage />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
+
+// * 최신 라우터
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ udemy-react/library/react-router/src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductPage() {
+  return <h1>The Products Pages</h1>
+}
+
+export default ProductPage;
